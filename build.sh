440234d1ad1ff631b43c271ab047ea381452b4cd
Adakai <abovethenegativity23@gmail.com>
2020-02-20 08:47:14 -0700

    fixed bash script

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+git clone https://github.com/Adakai/9-project.git
+
+cd 9-project
 
 docker build -t docker-app/node-web-app .
 
-docker run -p 49160:8080 -d docker-app/node-web-app
+# docker run -p 8080:8080 -d docker-app/node-web-app
 
 docker-compose up
